813a700e98e120f8609e0b795bfe12bab5a1b081
kramsayu <Professor8909@gmail.com>
2019-12-06 00:00:03 +0200

    lab_3: server.log

--- lab_3/main/views.py
+++ lab_3/main/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import os
+from datetime import datetime
 
-# Create your views here.
+
+def main(request):
+    return render(request, 'main.html', {'parameter': "kramsayu"})
+
+
+def health(request):
+    dt = datetime.now()
+    response = {
+            'date': dt.strftime("%Y-%m-%d %H:%M-%S"),
+            'current_page': request.path,
+            'server_info': os.uname(), 
+            'client_info': request.META['HTTP_USER_AGENT']
+    }
+    return JsonResponse(response)
+
